cb9dfc22798374bf1fc45751b281c83206bb636e
Brian Hart <brian.hart@xylogix.net>
2021-02-07 11:48:59 -0500

    Implement the InsertQuarter() method

--- GumballMachine/GumballMachineContext.cs
+++ GumballMachine/GumballMachineContext.cs
@@ -47,7 +47,32 @@ namespace GumballMachine
       /// </summary>
       public void InsertQuarter()
       {
-         throw new NotImplementedException();
+         switch (State)
+         {
+            case HAS_QUARTER: // There already is a quarter in the machine.
+               Console.WriteLine("You can't insert another quarter.");
+               break;
+
+            case NO_QUARTER: // A quarter isn't in the machine.
+               State = HAS_QUARTER;
+               Console.WriteLine("You inserted a quarter.");
+               break;
+
+            case SOLD_OUT: // No more gumballs are left.
+               Console.WriteLine(
+                  "You can't insert a quarter because the machine is sold out."
+               );
+               break;
+               ;
+
+            case SOLD: // A gumball was dispensed.
+               Console.WriteLine(
+                  "Please wait.  We're already giving you a gumball."
+               );
+               break;
+         }
       }
    }
+}
+
 }
