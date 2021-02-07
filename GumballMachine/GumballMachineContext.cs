a10f681b63440aa5ee6cd8ee109aeaf069103b75
Brian Hart <brian.hart@xylogix.net>
2021-02-07 11:41:53 -0500

    Create property State

--- GumballMachine/GumballMachineContext.cs
+++ GumballMachine/GumballMachineContext.cs
@@ -1,6 +1,4 @@
-﻿using System.Net.NetworkInformation;
-
-namespace GumballMachine
+﻿namespace GumballMachine
 {
    /// <summary>
    /// Represents a Gumball Machine.
@@ -8,9 +6,9 @@ namespace GumballMachine
    public class GumballMachineContext
    {
       /// <summary>
-      /// The machine is out of gumballs.
+      /// A quarter has been inserted in the machine.
       /// </summary>
-      public const int SOLD_OUT = 0;
+      public const int HAS_QUARTER = 2;
 
       /// <summary>
       /// There is no quarter in the machine.
@@ -18,14 +16,19 @@ namespace GumballMachine
       public const int NO_QUARTER = 1;
 
       /// <summary>
-      /// A quarter has been inserted in the machine.
-      /// </summary>
-      public const int HAS_QUARTER = 2;
-
-      /// <summary>
       /// A gumball has been dispensed.
       /// </summary>
       public const int SOLD = 3;
+
+      /// <summary>
+      /// The machine is out of gumballs.
+      /// </summary>
+      public const int SOLD_OUT = 0;
+
+      /// <summary>
+      /// Gets or sets the current state.
+      /// </summary>
+      public int State { get; set; }
 
       /// <summary>
       /// Entry point of the program.
