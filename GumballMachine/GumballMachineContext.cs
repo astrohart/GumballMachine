1fcea5044a3985b2b5eebde946951279e0ea6149
Brian Hart <brian.hart@xylogix.net>
2021-02-07 11:41:25 -0500

    Create state variables

--- GumballMachine/GumballMachineContext.cs
+++ GumballMachine/GumballMachineContext.cs
@@ -1,10 +1,32 @@
-﻿namespace GumballMachine
+﻿using System.Net.NetworkInformation;
+
+namespace GumballMachine
 {
    /// <summary>
    /// Represents a Gumball Machine.
    /// </summary>
    public class GumballMachineContext
    {
+      /// <summary>
+      /// The machine is out of gumballs.
+      /// </summary>
+      public const int SOLD_OUT = 0;
+
+      /// <summary>
+      /// There is no quarter in the machine.
+      /// </summary>
+      public const int NO_QUARTER = 1;
+
+      /// <summary>
+      /// A quarter has been inserted in the machine.
+      /// </summary>
+      public const int HAS_QUARTER = 2;
+
+      /// <summary>
+      /// A gumball has been dispensed.
+      /// </summary>
+      public const int SOLD = 3;
+
       /// <summary>
       /// Entry point of the program.
       /// </summary>
