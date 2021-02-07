b77a76e0c33b66399fa8690f50acb62fd17c8113
Brian Hart <brian.hart@xylogix.net>
2021-02-07 11:39:43 -0500

    Add XML docs

--- GumballMachine/GumballMachineContext.cs
+++ GumballMachine/GumballMachineContext.cs
@@ -1,7 +1,16 @@
 ﻿namespace GumballMachine
 {
+   /// <summary>
+   /// Represents a Gumball Machine.
+   /// </summary>
    public class GumballMachineContext
    {
+      /// <summary>
+      /// Entry point of the program.
+      /// </summary>
+      /// <param name="args">
+      /// Collection of arguments passed on the command line of the program.
+      /// </param>
       public static void Main(string[] args) { }
    }
 }
