0f6c4f01974dbcb82465943e963bbaf026124070
Brian Hart <brian.hart@xylogix.net>
2021-02-07 12:43:14 -0500

    Create test harness skeleton

---
+++ GumballMachine.Tests/GumballMachineContextTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace GumballMachine.Tests
+{
+   /// <summary>
+   /// Provides unit tests.
+   /// </summary>
+   [TestFixture]
+   public class GumballMachineContextTests
+   {
+      // TODO: Add test fixture implementation here
+   }
+}
