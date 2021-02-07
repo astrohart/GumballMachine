784fe872d577b82fa44ac6143c911268db9863b6
Brian Hart <brian.hart@xylogix.net>
2021-02-07 12:53:50 -0500

    Implement Test_GumballMachine_WorksAsExpected_ForNormalUse unit test

--- GumballMachine.Tests/GumballMachineContextTests.cs
+++ GumballMachine.Tests/GumballMachineContextTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace GumballMachine.Tests
 {
@@ -14,7 +15,56 @@ namespace GumballMachine.Tests
       [Test]
       public void Test_GumballMachine_WorksAsExpected_ForNormalUse()
       {
-         // TODO: Add unit test statements here.
+         const int INITIAL_GUMBALL_AMOUNT = 5;
+
+         // create a new gumball machine loaded with INITIAL_GUMBALL_AMOUNT gumballs.
+         var machine = new GumballMachineContext(INITIAL_GUMBALL_AMOUNT);
+
+         Console.WriteLine(machine); // print out the state of the machine
+
+         machine.InsertQuarter(); // throw a quarter in...
+         machine.TurnCrank(); // turn the crank; we should get one gumball.
+
+         // since we inserted a quarter and then turned the crank, we expect
+         // that the machine has one less gumball than we loaded it with
+         Assert.IsTrue(machine.Count == INITIAL_GUMBALL_AMOUNT - 1);
+
+         Console.WriteLine(machine); // print out the state of the machine again
+
+         machine.InsertQuarter(); // throw a quarter in...
+         machine.EjectQuarter(); // ask for it back
+         machine.TurnCrank(); // turn he crank; we shouldn't get a gumball
+
+         // should still have INITIAL_GUMBALL_COUNT - 1 gumballs in the machine
+         Assert.IsTrue(machine.Count == INITIAL_GUMBALL_AMOUNT - 1);
+
+         Console.WriteLine(machine); // print out the state of the machine again
+
+         machine.InsertQuarter(); // throw a quarter in...
+         machine.TurnCrank(); // turn the crank; we should get one gumball.
+         machine.InsertQuarter(); // throw a quarter in...
+         machine.TurnCrank(); // turn the crank; we should get one gumball.
+         machine.EjectQuarter(); // ask for a quarter back that we didn't put in.
+
+         // should now have even less gumballs (by two more) than we started
+         // with prior to the most recently run group of tests
+         Assert.IsTrue(machine.Count == INITIAL_GUMBALL_AMOUNT - 3);
+
+         Console.WriteLine(machine); // print out the state of the machine again
+
+         machine.InsertQuarter(); // throw TWO quarters in...
+         machine.InsertQuarter();
+         machine.TurnCrank(); // turn the crank; we should get one gumball.
+         machine.InsertQuarter();
+         machine.TurnCrank(); // now for the stress test :-)
+         machine.InsertQuarter();
+         machine.TurnCrank();
+
+         Assert.IsTrue(
+            machine.Count <= 0
+         ); // assert that machine is out of inventory
+
+         Console.WriteLine(machine); // print that machine state one more time
       }
    }
 }
