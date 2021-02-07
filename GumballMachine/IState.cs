e12a4b0a425cb652f40f4f802f2e075b2dd4c93f
Brian Hart <brian.hart@xylogix.net>
2021-02-07 13:52:18 -0500

    Add action methods

--- GumballMachine/IState.cs
+++ GumballMachine/IState.cs
@@ -3,5 +3,26 @@
    /// <summary>
    /// Defines the behaviors of the gumball machine when it's in a given state.
    /// </summary>
-   public interface IState { }
+   public interface IState
+   {
+      /// <summary>
+      /// Inserts a quarter into the gumball machine.
+      /// </summary>
+      void InsertQuarter();
+
+      /// <summary>
+      /// Ejects a quarter from the gumball machine.
+      /// </summary>
+      void EjectQuarter();
+
+      /// <summary>
+      /// Turns the crank of the gumball machine.
+      /// </summary>
+      void TurnCrank();
+
+      /// <summary>
+      /// Called to dispense a gumball.
+      /// </summary>
+      void Dispense();
+   }
 }
